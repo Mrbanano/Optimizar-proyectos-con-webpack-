8a423efd54f38dc70991d861cadebf61db7b46da
Alvaro castillo <6666alvaro666@gmail.com>
2021-06-29 11:59:52 -0500

    loader de fuentes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlwebPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
     extensions: [".js"],
@@ -33,7 +34,21 @@ module.exports = {
           {
             test: /\.png$/,
             type:'asset/resource'
-          }
+          },
+          {
+            test: /\.(woff|woff2)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 10000,
+                mimetype: "application/font-woff",
+                name: "[name].[ext]",
+                outputPath: "./assets/fonts/",
+                publicPath: "./assets/fonts/",
+                esModule: false,
+              },
+            }
+          },
       ]
   },
   plugins:[
